22bf291cb0ec5389e553bdc44b9fd1ee29204de7
Ethan <>
2022-04-22 11:10:13 -0500

    Fixed BFS and added queeue.  Functions as intended

--- elevator.c
+++ elevator.c
@@ -5,8 +5,19 @@
 //Keeps Track of connections
 int Graph[MAXFLOORS][MAXFLOORS];
 void SetupGraph(int floors,int Up, int Down);
-int* BST(int start,int goal,int floors);
+void BFS(int start,int goal,int floors);
 
+struct queue
+{
+   int items[MAXFLOORS];
+   int front;
+   int rear;
+};
+
+struct queue* MakeQueue();
+void enqueue(struct queue* q, int);
+int dequeue(struct queue* q);
+int isEmpty(struct queue* q);
 
 
 int main(void)
@@ -40,23 +51,8 @@ int main(void)
 
     SetupGraph(floors,up,down);
 
-    /*
-    for(int i = 0;i < floors;i++)
-    {
-        printf("Graph[%d] connections: ",i);
-        for(int j = 0; j<floors;j++)
-        {
-            if(Graph[i][j] == 1)
-                printf("%d ,",j);
-        }
-            
+    BFS(start,goal,floors);
 
-        printf("\n");
-    }
-    */
-
-    int * hold = BST(start,goal,floors);
-    
 
 }
 
@@ -84,30 +80,100 @@ void SetupGraph(int floors,int Up, int Down)
         if(downHold >= 0)
         {
             Graph[i][downHold] = 1;
-        } 
+        }
     }
 }
 
-int* BST(int start,int goal,int floors)
+void BFS(int start,int goal,int floors)
 {
-    int queue[floors], visited[floors];
-    int rear = -1;
-    int front = 0;
-    int queueCount = 0;
-    int visitedcount = 0;
-
-    visited[visitedcount] = start;
-
-    for(int i = 0; i<floors ;i++)
+   //Make que and array to keep track of where weve been
+    struct queue * Que = MakeQueue();
+    int visited[floors];
+    int parent[floors];
+    for(int i = 0;i<floors;i++)
     {
-        if(Graph[start][i] == 1)
-        {
-            printf("adding %d to queue and the start is %d\n",i, start);
-            queue[front] = i;
-            front++;
-        }
+      parent[i] = 0;
+      visited[i] = 0;
     }
+    visited[start-1] = 1;
+    enqueue(Que,start-1);
+    while(isEmpty(Que) != 1)
+    {
+      int current = dequeue(Que);
+      if(current +1 == goal)
+      {
+         for(int i = current;i != start-1;i = i)
+         {
+            printf("%d <- ",i+1);
+            i = parent[i] ;
+         }
+         printf("%d\n",start);
+         return;
+      }
 
+      //enquee the other nodes
+      for(int i = 0;i<floors;i++)
+      {
+         //only que if other hasnt been visited current
+         if(visited[i] == 0 && Graph[current][i] == 1)
+         {
+            parent[i] = current;
+            visited[i] = 1;
+            enqueue(Que,i);
+         }
+      }
+    }
+    printf("Take the Stairs\n");
+    free(Que);
+}
 
+struct queue* MakeQueue()
+{
+   struct queue* q = malloc(sizeof(struct queue));
+   q->front = -1;
+   q->rear = -1;
+   return q;
+}
 
+int isEmpty(struct queue* q)
+{
+   //Check to see if rear is -1
+   if(q->rear == -1)
+      return 1;
+   return 0;
+}
+
+void enqueue(struct queue* q, int data)
+{
+   if(q->rear < MAXFLOORS -1)
+   {
+      //check to see if empty first
+      if(q->front == -1)
+      {
+         q->front = 0;
+      }
+      q->rear++;
+      q->items[q->rear] = data;
+   }
+}
+
+int dequeue(struct queue* q)
+{
+   int hold;
+   //Check to see if empty first
+   if(isEmpty(q))
+   {
+      printf("error empty que\n");
+      return -1;
+   }
+   else
+   {
+      hold = q->items[q->front];
+      q->front++;
+      if(q->front > q->rear)
+      {
+         q->front = q->rear = -1;
+      }
+   }
+   return hold;
 }
